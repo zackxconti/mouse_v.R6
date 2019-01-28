f54020e3c9e1b258bfec42b77dbffe1b3b509429
chepilot <liruizenus@gmail.com>
2019-01-27 20:20:12 -0500

    synchronize with Rui's UI part

--- src/Lab_Mouse_vR6/Components/POutput.cs
+++ src/Lab_Mouse_vR6/Components/POutput.cs
@@ -13,29 +13,55 @@ using Grasshopper.GUI.Canvas;
 using Grasshopper.Kernel;
 using Grasshopper.Kernel.Types;
 using Grasshopper.Kernel.Attributes;
-using Lab_Mouse_vR6.Properties;
-using Lab_Mouse_vR6.Components;
+using Lab_Mouse.Components;
 
-namespace Lab_Mouse_vR6.Components
+namespace Lab_Mouse.Components
 {
-    public class POutput : GH_Param<GH_Integer>
+    public class POutput : GH_Param<GH_Number>
     {
         /// Initializes a new instance of the MyComponent1 class.
         public List<double> probabilities;
         // default starting string, need to be the same as the default starting probability distribution
-        public string tempPD = "0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1";
+        public string tempPD = "0.0";
         public float max;
         public float min;
+        public List<GH_Number> userInput;
         public string draw_flag;
 
         public POutput()
           : base(new GH_InstanceDescription("PDF Output", "POutput",
               "bla bla",
-              "Lab Mouse vR6", "Modeling"))
+              "Lab Mouse", "Modeling"))
         {
             // default starting distribution
-            this.probabilities = new List<double> { 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 };
+            this.probabilities = new List<double> { 0.0 };
             this.draw_flag = "h";
+        }
+
+        // get user input and construct default probability list 
+        public bool getUserInput()
+        {
+            if (VolatileData.IsEmpty) {
+                this.probabilities = new List<double> { 0.0 };
+                this.userInput = new List<GH_Number> { new GH_Number(0.0) };
+                return false;
+            }
+
+            this.userInput = this.m_data[0];
+            List <double> temp = new List<double>();
+            for(int i =0; i < this.userInput.Count; i++)
+            {
+                if (i < this.probabilities.Count)
+                {
+                    temp.Add(this.probabilities[i]);
+                }
+                else
+                {
+                    temp.Add(0.0);
+                }
+            }
+            this.probabilities = temp;
+            return true;
         }
 
         public override GH_Exposure Exposure
@@ -136,7 +162,6 @@ namespace Lab_Mouse_vR6.Components
             }
 
             this.probabilities = tempPDList;
-
             ExpireSolution(true);
         }
 
@@ -187,14 +212,14 @@ namespace Lab_Mouse_vR6.Components
         private PointF[] getPts(List<double> Probabilities)
 
         {
-            int n = probabilities.Count + 4;
+            int n = Probabilities.Count + 4;
             PointF[] points = new PointF[n];
 
             points[0] = new PointF(this.Pivot.X, this.Pivot.Y - 7);
             points[1] = new PointF(this.Pivot.X + (Bounds.Width) - 2, this.Pivot.Y - 7);
 
 
-            if (probabilities.Count != 0)
+            if (Probabilities.Count != 0)
             {
                 // routine to get drawing coordinates based on bin  probabilities
                 for (int i = 0; i < Probabilities.Count; i++)
@@ -218,30 +243,59 @@ namespace Lab_Mouse_vR6.Components
         private RectangleF[] getBackgroundBins(List<double> Probabilities)
         {
             int n = Probabilities.Count;
-            RectangleF[] backgroundBins = new RectangleF[n];
-
-            PointF[] points = new PointF[n];
-
-
-            points[0] = new PointF(this.Pivot.X, this.Pivot.Y - 7);
-            points[1] = new PointF(this.Pivot.X + (Bounds.Width) - 2, this.Pivot.Y - 7);
-
-            if (Probabilities.Count != 0)
+            if (n != 1)
             {
-                for (int i = 0; i < Probabilities.Count; i++)
-                {
-                    float rail_width = (int)(points[1].X - points[0].X);
-                    float bin_width = rail_width / Probabilities.Count;
+                RectangleF[] backgroundBins = new RectangleF[n];
 
-                    backgroundBins[i] = new System.Drawing.RectangleF(
-                        (float)((this.Pivot.X) + (bin_width * (Probabilities.Count - i - 1))),
-                        (float)(this.Pivot.Y - 7 - glob.max_ht),
-                        (float)bin_width,
-                        (float)(glob.max_ht));
+                PointF[] points = new PointF[n];
+
+
+                points[0] = new PointF(this.Pivot.X, this.Pivot.Y - 7);
+                points[1] = new PointF(this.Pivot.X + (Bounds.Width) - 2, this.Pivot.Y - 7);
+
+                if (Probabilities.Count != 0)
+                {
+                    for (int i = 0; i < Probabilities.Count; i++)
+                    {
+                        float rail_width = (int)(points[1].X - points[0].X);
+                        float bin_width = rail_width / Probabilities.Count;
+
+                        backgroundBins[i] = new System.Drawing.RectangleF(
+                            (float)((this.Pivot.X) + (bin_width * (Probabilities.Count - i - 1))),
+                            (float)(this.Pivot.Y - 7 - glob.max_ht),
+                            (float)bin_width,
+                            (float)(glob.max_ht));
+                    }
                 }
+                return backgroundBins;
+            }
+            else
+            {
+                RectangleF[] backgroundBins = new RectangleF[n];
+
+                PointF[] points = new PointF[n];
+
+
+                points[0] = new PointF(this.Pivot.X, this.Pivot.Y - 7);
+                PointF point = new PointF(this.Pivot.X + (Bounds.Width) - 2, this.Pivot.Y - 7);
+
+                if (Probabilities.Count != 0)
+                {
+                    for (int i = 0; i < Probabilities.Count; i++)
+                    {
+                        float rail_width = (int)(point.X - points[0].X);
+                        float bin_width = rail_width / Probabilities.Count;
+
+                        backgroundBins[i] = new System.Drawing.RectangleF(
+                            (float)((this.Pivot.X) + (bin_width * (Probabilities.Count - i - 1))),
+                            (float)(this.Pivot.Y - 7 - glob.max_ht),
+                            (float)bin_width,
+                            (float)(glob.max_ht));
+                    }
+                }
+                return backgroundBins;
             }
 
-            return backgroundBins;
         }
 
         // Calculate the background bays for the graph based on Probabilities 
@@ -251,28 +305,53 @@ namespace Lab_Mouse_vR6.Components
             int n = Probabilities.Count;
             RectangleF[] textholder = new RectangleF[n];
 
-            PointF[] points = new PointF[n];
-
-            float bin_width = (Bounds.Width-4 )/ Probabilities.Count;
-
-            Rhino.RhinoApp.WriteLine(bin_width.ToString());
-
-            points[0] = new PointF(this.Pivot.X+2, this.Pivot.Y + 62);
-            points[1] = new PointF(this.Pivot.X + (Bounds.Width) - bin_width, this.Pivot.Y + 62);
-
-            if (Probabilities.Count != 0)
+            if (n != 1)
             {
-                for (int i = 0; i < Probabilities.Count; i++)
-                {
-                    textholder[i] = new System.Drawing.RectangleF(
-                        (float)((this.Pivot.X+3) + (bin_width * (Probabilities.Count - i - 1))),
-                        (float)(this.Pivot.Y + 2),
-                        (float)(bin_width-2),
-                        (float)(16));
-                }
-            }
+                PointF[] points = new PointF[n];
 
-            return textholder;
+                float bin_width = (Bounds.Width - 4) / Probabilities.Count;
+
+                points[0] = new PointF(this.Pivot.X + 2, this.Pivot.Y + 62);
+                points[1] = new PointF(this.Pivot.X + (Bounds.Width) - bin_width, this.Pivot.Y + 62);
+
+                if (Probabilities.Count != 0)
+                {
+                    for (int i = 0; i < Probabilities.Count; i++)
+                    {
+                        textholder[i] = new System.Drawing.RectangleF(
+                            (float)((this.Pivot.X + 3) + (bin_width * (Probabilities.Count - i - 1))),
+                            (float)(this.Pivot.Y + 2),
+                            (float)(bin_width - 2),
+                            (float)(16));
+                    }
+                }
+
+                return textholder;
+            }
+            else
+            {
+                PointF[] points = new PointF[n];
+
+                float bin_width = (Bounds.Width - 4) / Probabilities.Count;
+
+                points[0] = new PointF(this.Pivot.X + 2, this.Pivot.Y + 62);
+                PointF point = new PointF(this.Pivot.X + (Bounds.Width) - bin_width, this.Pivot.Y + 62);
+
+                if (Probabilities.Count != 0)
+                {
+                    for (int i = 0; i < Probabilities.Count; i++)
+                    {
+                        textholder[i] = new System.Drawing.RectangleF(
+                            (float)((this.Pivot.X + 3) + (bin_width * (Probabilities.Count - i - 1))),
+                            (float)(this.Pivot.Y + 2),
+                            (float)(bin_width - 2),
+                            (float)(16));
+                    }
+                }
+
+                return textholder;
+            }
+            
         }
 
         // this function gets the coordinates from the probabilities, (same as getPts) but draws a HISTOGRAM shape an irregular polygon from points
@@ -282,27 +361,54 @@ namespace Lab_Mouse_vR6.Components
             int n = (Probabilities.Count * 2) + 2;
             PointF[] points = new PointF[n];
 
-            points[0] = new PointF(this.Pivot.X, this.Pivot.Y - 7);
-            points[1] = new PointF(this.Pivot.X + (Bounds.Width) - 2, this.Pivot.Y - 7);
-
-
-            if (Probabilities.Count != 0)
+            if (n != 1)
             {
-                int count = 0;
-                // routine to get drawing coordinates based on bin  probabilities
-                for (int i = 0; i < Probabilities.Count; i++)
-                {
-                    float rail_width = (int)(points[1].X - points[0].X);
-                    float bin_width = rail_width / Probabilities.Count;
-                    float t = (Probabilities.Count - i) / Probabilities.Count;
+                points[0] = new PointF(this.Pivot.X, this.Pivot.Y - 7);
+                points[1] = new PointF(this.Pivot.X + (Bounds.Width) - 2, this.Pivot.Y - 7);
 
-                    points[count + 2] = new PointF((float)((this.Pivot.X) + (bin_width * (Probabilities.Count - i))), this.Pivot.Y - 7 - (glob.max_ht * (float)Probabilities[Probabilities.Count - i - 1]));
-                    points[count + 2 + 1] = new PointF((float)((this.Pivot.X) + (bin_width * (Probabilities.Count - i - 1))), this.Pivot.Y - 7 - (glob.max_ht * (float)Probabilities[Probabilities.Count - i - 1]));
-                    count += 2;
+
+                if (Probabilities.Count != 0)
+                {
+                    int count = 0;
+                    // routine to get drawing coordinates based on bin  probabilities
+                    for (int i = 0; i < Probabilities.Count; i++)
+                    {
+                        float rail_width = (int)(points[1].X - points[0].X);
+                        float bin_width = rail_width / Probabilities.Count;
+                        float t = (Probabilities.Count - i) / Probabilities.Count;
+
+                        points[count + 2] = new PointF((float)((this.Pivot.X) + (bin_width * (Probabilities.Count - i))), this.Pivot.Y - 7 - (glob.max_ht * (float)Probabilities[Probabilities.Count - i - 1]));
+                        points[count + 2 + 1] = new PointF((float)((this.Pivot.X) + (bin_width * (Probabilities.Count - i - 1))), this.Pivot.Y - 7 - (glob.max_ht * (float)Probabilities[Probabilities.Count - i - 1]));
+                        count += 2;
+                    }
                 }
+                return points;
+            }
+            else
+            {
+                points[0] = new PointF(this.Pivot.X, this.Pivot.Y - 7);
+                PointF point = new PointF(this.Pivot.X + (Bounds.Width) - 2, this.Pivot.Y - 7);
+
+
+                if (Probabilities.Count != 0)
+                {
+                    int count = 0;
+                    // routine to get drawing coordinates based on bin  probabilities
+                    for (int i = 0; i < Probabilities.Count; i++)
+                    {
+                        float rail_width = (int)(point.X - points[0].X);
+                        float bin_width = rail_width / Probabilities.Count;
+                        float t = (Probabilities.Count - i) / Probabilities.Count;
+
+                        points[count + 2] = new PointF((float)((this.Pivot.X) + (bin_width * (Probabilities.Count - i))), this.Pivot.Y - 7 - (glob.max_ht * (float)Probabilities[Probabilities.Count - i - 1]));
+                        points[count + 2 + 1] = new PointF((float)((this.Pivot.X) + (bin_width * (Probabilities.Count - i - 1))), this.Pivot.Y - 7 - (glob.max_ht * (float)Probabilities[Probabilities.Count - i - 1]));
+                        count += 2;
+                    }
+                }
+                return points;
             }
 
-            return points;
+
         }
 
         // double click event handler
@@ -332,11 +438,13 @@ namespace Lab_Mouse_vR6.Components
                     {
                         own.probabilities[pos] = 1;
                     }
+
                     Owner.OnDisplayExpired(true);
                     return GH_ObjectResponse.Handled;
                 }
             }
 
+            Owner.OnDisplayExpired(true);
             return base.RespondToMouseDoubleClick(sender, e);
         }
 
@@ -375,14 +483,26 @@ namespace Lab_Mouse_vR6.Components
 
             // Create a new Capsule 
             GH_Capsule capsule = GH_Capsule.CreateCapsule(new RectangleF(this.Pivot, new SizeF(Bounds.Width, 20)), palette);
-            capsule.AddInputGrip(this.InputGrip.X, this.InputGrip.Y);
-            capsule.AddOutputGrip(this.OutputGrip.X, this.OutputGrip.Y);
+            capsule.AddInputGrip(this.InputGrip.Y);
+            capsule.AddOutputGrip(this.OutputGrip.Y);
+
+            string componentName;
+
+            if(own.NickName == "")
+            {
+                componentName = "Probabilities";
+            }else
+            {
+                componentName = "Probabilities (" + own.NickName + " )";
+            }
+
+            bool validInput = own.getUserInput();
 
             GH_Capsule message = GH_Capsule.CreateTextCapsule(
                 new RectangleF(new PointF(this.Pivot.X, this.Pivot.Y + 20), new SizeF(Bounds.Width, 20)),
                 new RectangleF(new PointF(this.Pivot.X, this.Pivot.Y + 20), new SizeF(Bounds.Width, 20)),
                 GH_Palette.Hidden,
-                "Probabilities"
+                componentName
                 );
 
             message.Render(graphics, Selected, Owner.Locked, false);
@@ -455,7 +575,7 @@ namespace Lab_Mouse_vR6.Components
 
             for (int i =0; i < textCapsules.Length; i++)
             {
-                textCapsules[i] = GH_Capsule.CreateTextCapsule(textholderBounds[i], textholderBounds[i], GH_Palette.Hidden, "<=" + "48.3",3,3);
+                textCapsules[i] = GH_Capsule.CreateTextCapsule(textholderBounds[i], textholderBounds[i], GH_Palette.Hidden, "<=" + own.userInput[textCapsules.Length-i-1].Value.ToString(),3,3);
                 textCapsules[i].Render(graphics, Selected, Owner.Locked, false);
             }
         }
